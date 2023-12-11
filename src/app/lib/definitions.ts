5dd7f5f834ebff3d5233461dc710bcb2c78d6368
ricardo <rblazquez111@gmail.com>
2023-12-10 22:34:35 -0300

    Dashboard Login

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -1,12 +1,26 @@
 export type Products = {
-    id: string;
-    model: string;
-    category: string;
-    specs: object;
-    image: string;
-    colors: [];
-    price: string
-    carrusel: object;
-    video: string;
-    website: string;
-  };
+  id: string;
+  model: string;
+  category: string;
+  specs: object;
+  image: string;
+  colors: [];
+  price: string
+  carrusel: object;
+  video: string;
+  website: string;
+};
+
+export interface LoginInterface {
+  email: string,
+  password: string,
+};
+
+export interface InputInterface {
+  type: string,
+  name: string,
+  handler: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  value: string,
+  autoComplete: string,
+  errors: string | undefined,
+}
