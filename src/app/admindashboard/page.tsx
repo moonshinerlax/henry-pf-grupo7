2c9469432afc94c171921c2325a0206cd6628a93
Aricoins <arielgarcia79@gmail.com>
2024-01-19 15:40:12 +0000

    dashLoading

--- src/app/admindashboard/page.tsx
+++ src/app/admindashboard/page.tsx
@@ -5,7 +5,9 @@ import { checkUserRole } from "@/app/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
- 
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 interface Products{
     id: string;
     model: string;
@@ -26,6 +28,7 @@ export default function admindashboard() {
     const [productList, setProductList] = useState([])
     const [users, setUsers] = useState([])
     const [status, setStatus] = useState(0);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
             const fetchProducts = async () => {
@@ -36,6 +39,7 @@ export default function admindashboard() {
                     setProductList(products.products);
                     setUsers(products.users);
                     console.log(products)
+                    setLoading(true)
                   } else {
                     console.error('Failed to fetch products:', response.statusText);
                   }
@@ -44,7 +48,7 @@ export default function admindashboard() {
                 }
               };
         fetchProducts();
-       
+     
     }, [status]);
 
     const toggleCheckbox = (id: string, disable: boolean) => {
@@ -80,13 +84,18 @@ export default function admindashboard() {
     }
     updateDisableStatus()
   }
-    
+useEffect(() => {
+  Aos.init({ duration: 1000 });
+})
+  
+
   return (
-    <div>
+    !loading ? <h1 data-aos="fade-out">Loading...</h1> : 
+  <div>
         {userRole === 'org:admin' ? 
          <div className=" md:flex md:flex-row w-full place-content-evenly">
 
-        <div className="rounded-lg h-auto md:h-28 md:mt-64 bg-slate-100 bg-opacity-70 shadow-[0_0px_10px_5px_rgba(100,100,100,.8)] m-1 px-2">
+        <div data-aos="fade-up"  data-aos-duration="1000" className="rounded-lg h-auto md:h-28 md:mt-64 bg-slate-100 bg-opacity-70 shadow-[0_0px_10px_5px_rgba(100,100,100,.8)] m-1 px-2">
           <Link href={'/form'}>
           <button className="bg-blue-500 mx-24 my-10 rounded-full p-4 self-center md:m-7 hover:bg-blue-800 ">
             Add New Product
@@ -95,7 +104,7 @@ export default function admindashboard() {
         </div>
 
 
-          <div className="rounded-3xl shadow-[0_0px_10px_5px_rgba(100,100,100,.5)] m-1">
+          <div  data-aos="fade-up"  className=" rounded-3xl shadow-[0_0px_10px_5px_rgba(100,100,100,.5)] m-1">
           <div className="min-w-full h-screen grid grid-cols-3 gap-5">
           <div className="ovewrflow-y-scroll overflow-x-hidden  text-xs w-96 h-auto mr-1 md:col-span-3 md:w-auto">
             <table className="min-w-full bg-slate-100 bg-opacity-80 rounded-lg">
@@ -137,10 +146,8 @@ export default function admindashboard() {
           </div>
         </div>
         </div>
-      
-
-           
-           <div className="rounded-lg shadow-[0_0px_10px_5px_rgba(100,100,100,.5)] m-1"> 
+                
+           <div  data-aos="fade-up"  className="rounded-lg shadow-[0_0px_10px_5px_rgba(100,100,100,.5)] m-1"> 
                              <div className="min-w-full h-screen grid md:grid-cols-3 md:gap-5">
                     <div className="ovewrflow-y-scroll overflow-x-hidden h-full mr-1 md:col-span-3 w-96 md:w-auto">
             <table className=" h-full bg-slate-100 bg-opacity-80 rounded-lg">
@@ -173,6 +180,5 @@ export default function admindashboard() {
         </div> 
          : 
          <h1>Access Not Granted!</h1>}
-      </div>
-  );
+      </div>);
 }
